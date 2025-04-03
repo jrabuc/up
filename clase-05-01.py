ab6acea31013fc04fd9663afc057474ecb8d481a
jrabuch <jrabuch@gmail.com>
2025-04-03 20:18:41 -0300

    Add sumar function and refactor ordenar function in clase-05-01.py

--- clase-05-01.py
+++ clase-05-01.py
@@ -1,5 +1,9 @@
-def ordenar(arreglo):
-    for i in range(len(arreglo)-3):
+def sumar(a, b):
+    return a + b    
+
+
+def ordenar_arreglo(arreglo):
+    for i in range(len(arreglo)):
         for j in range(i + 1, len(arreglo)):
             if arreglo[i] > arreglo[j]:
                 arreglo[i], arreglo[j] = arreglo[j], arreglo[i]
